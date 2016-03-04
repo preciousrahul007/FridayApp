8f6ea39361be7f44e7846c4d1f6678c56870556a
student <student@Achiever93.local>
2016-03-04 18:21:08 +0530

    Initial Commit

---
+++ FridayApp/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  FridayApp
+//
+//  Created by student on 04/03/16.
+//  Copyright (c) 2016 bnr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ FridayApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FridayApp
+//
+//  Created by student on 04/03/16.
+//  Copyright (c) 2016 bnr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
